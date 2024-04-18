ba965bd1be1b58993e04d7b6c92b2f52288d0265
Sergei Dmitriev <iserdmi@gmail.com>
2024-04-18 18:27:36 +0400

    Small fix

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,8 @@ import {
 } from 'svag-cli-utils'
 import z from 'zod'
 
-defineCliApp(async ({ cwd, command, flags, argr }) => {
+defineCliApp(async ({ cwd, command, args, flags, argr }) => {
+  cwd = path.resolve(cwd, args[0] || '.')
   const { packageJsonDir, packageJsonPath } = await getPackageJson({ cwd })
 
   const createConfigFile = async () => {
